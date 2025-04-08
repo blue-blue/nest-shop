96ca84b7f4a981cecd347d80cdb2f848f0a3ac80
chuchengxiang <shs-cyhlwzyccx@chinaunicom.cn>
2025-04-08 17:31:35 +0800

    feat: global exec filter

--- src/common/filters/all-execption.filter.ts
+++ src/common/filters/all-execption.filter.ts
@@ -1,6 +1,9 @@
-import { ArgumentsHost, Catch, ExceptionFilter } from '@nestjs/common';
+import { ArgumentsHost, Catch, ExceptionFilter, Logger } from '@nestjs/common';
+import { HttpAdapterHost } from '@nestjs/core';
 
 @Catch()
 export class AllExecptionFilter<T> implements ExceptionFilter {
+  private readonly logger = new Logger();
+  constructor(private readonly httpAdapterHost: HttpAdapterHost) {}
   catch(exception: T, host: ArgumentsHost) {}
 }
